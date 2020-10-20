b9336873ac0fa49cb88d58ddda7077795c14e231
BorekZnovustvoritel <73165420+BorekZnovustvoritel@users.noreply.github.com>
2020-10-20 11:46:49 +0200

    Add files via upload

---
+++ eightball.py
@@ -0,0 +1,12 @@
+from random import randint
+
+eightball = ["Ano","Ne","Možná","Spíš ne","Czechbol říká, že ne","Je vyšší šance, že dostaneš ban.","Smrdí ti nohy","Zeptej se později","Opravdu? Ne.","Zítra ráno","Ano... i když spíš ne.",":kek:"]
+print("Hello there, zeptej se mě na něco.\n")
+
+exitprog = False
+while exitprog != True:
+    a = input()
+    if a == 'exit':
+        exitprog = True
+    else:
+        print(eightball[randint(0,len(eightball)-1)])
