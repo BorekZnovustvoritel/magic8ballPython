65b0c99993c23da069b791f009f47d04593bb218
BorekZnovustvoritel <73165420+BorekZnovustvoritel@users.noreply.github.com>
2020-10-20 12:21:02 +0200

    Update eightball.py

--- eightball.py
+++ eightball.py
@@ -1,7 +1,7 @@
 from random import randint
 
-eightball = ["Ano","Ne","Možná","Spíš ne","Czechbol říká, že ne","Je vyšší šance, že dostaneš ban.","Smrdí ti nohy","Zeptej se později","Opravdu? Ne.","Zítra ráno","Ano... i když spíš ne.",":kek:"]
-print("Hello there, zeptej se mě na něco.\n\nPS: Až tě přestanu bavit, ukonči mě příkazem 'exit'.\n")
+eightball = [" Ano"," Ne"," Možná"," Spíš ne"," Czechbol říká, že ne"," Je vyšší šance, že dostaneš ban."," Smrdí ti nohy"," Zeptej se později"," Opravdu? Ne."," Zítra ráno"," Ano... i když spíš ne.",":kek:"]
+print(" Hello there, zeptej se mě na něco.\n\n PS: Až tě přestanu bavit, ukonči mě příkazem 'exit'.\n")
 
 exitprog = False
 while exitprog != True:
